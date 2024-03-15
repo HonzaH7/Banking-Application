339313d92a957922dba0741bb6257cfabd3116ba
Honza <janhrbac7@gmail.com>
2024-03-15 11:14:50 +0100

    added MoneyFlow instances

--- src/main/java/Banking/Application/BankingApplication.java
+++ src/main/java/Banking/Application/BankingApplication.java
@@ -4,12 +4,15 @@ import authentication.AuthenticationEventHandler;
 import authentication.AuthenticationService;
 import authentication.AuthenticationServiceImp;
 import datasource.DataSourceBean;
+import moneyFlow.MoneyFlowEventHandler;
+import moneyFlow.MoneyFlowService;
+import moneyFlow.MoneyFlowServiceImp;
+import org.modelmapper.ModelMapper;
 import userAccount.UserAccountManager;
 import utils.EventBroker.EventBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
 
 
 @SpringBootApplication
@@ -18,9 +21,15 @@ public class BankingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplication.class, args);
 		EventBroker eventBroker = new EventBroker();
+
 		DataSourceBean dataSourceBean = new DataSourceBean();
-		AuthenticationService authenticationService = new AuthenticationServiceImp(dataSourceBean, UserAccountManager.getInstance());
+
+		AuthenticationService authenticationService = new AuthenticationServiceImp(dataSourceBean, new ModelMapper() ,UserAccountManager.getInstance());
 		AuthenticationEventHandler authenticationEventHandler = new AuthenticationEventHandler(authenticationService, eventBroker);
+
+		MoneyFlowService moneyFlowService = new MoneyFlowServiceImp(dataSourceBean, UserAccountManager.getInstance());
+		MoneyFlowEventHandler moneyFlowEventHandler = new MoneyFlowEventHandler(moneyFlowService, eventBroker);
+
 		UserInterface userInterface = new CommandLineInterface(eventBroker);
 		userInterface.run();
 	}
